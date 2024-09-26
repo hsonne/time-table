290b79ba3f0578ca2050b67eeccffdd53259f782
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-09-26 04:41:28 +0200

    Get some insight into the data

--- main.R
+++ main.R
@@ -14,11 +14,12 @@ kwb.utils::loadFunctions(c(
   file_classes_1 <- "C:/Users/hsonne/Downloads/L/lernfoerderung/Klassen Neu 2024-2025.pdf"
   file_classes_2 <- "C:/Users/hsonne/Downloads/L/lernfoerderung/Klassen Neu2 024-2025.pdf"
   
-  full_teacher_data <- get_full_teacher_table(file = file_teachers)
-  full_class_data_1 <- get_full_class_table(file = file_classes_1)
-  full_class_data_2 <- get_full_class_table(file = file_classes_2)
+  teacher_data <- get_full_teacher_table(file = file_teachers)
+  class_data_1 <- get_full_class_table(file = file_classes_1)
+  class_data_2 <- get_full_class_table(file = file_classes_2)
 }
 
+# Have a look at the data ------------------------------------------------------
 {
   column_order <- c("weekday", "hr", "teacher", "class", "subject", "room")
   
@@ -28,9 +29,30 @@ kwb.utils::loadFunctions(c(
   
   my_print <- function(data) print(head(unify(data)))
   
-  my_print(full_teacher_data)
-  my_print(full_class_data_1)
-  my_print(full_class_data_2)
+  my_print(teacher_data)
+  my_print(class_data_1)
+  my_print(class_data_2)
+  
+  get_teachers <- function(data) sort(unique(data$teacher))
+  cmp <- kwb.utils::compareSets
+  
+  cmp(get_teachers(teacher_data), get_teachers(class_data_1))
+  cmp(get_teachers(class_data_2), get_teachers(class_data_1))
+  
+  data <- class_data_2
+  data$weekday_hr <- kwb.utils::pasteColumns(data, c("weekday", "hr"), "-")
+  
+  get_combis <- function(...) {
+    #kwb.utils::fullySorted(unique(kwb.utils::selectColumns(data, c(...))))
+    kwb.utils::countOrSum(data, c(...), .sum_up)
+  }
+  
+  get_combis("teacher", "subject")
+  get_combis("subject", "teacher")
+  get_combis("room", "subject")
+  get_combis("room", "weekday")
+  get_combis("weekday_hr", "room")
+  
 }
 
 # Write data to text files for comparison -------------------------------------- 
@@ -43,8 +65,8 @@ kwb.utils::loadFunctions(c(
     data[data$subject == "Deu", ]
   }
   
-  my_write(my_filter(full_class_data_1), "ems_classes_deu_1.txt")
-  my_write(my_filter(full_class_data_2), "ems_classes_deu_2.txt")
+  my_write(my_filter(class_data_1), "ems_classes_deu_1.txt")
+  my_write(my_filter(class_data_2), "ems_classes_deu_2.txt")
   
   #kwb.utils::hsOpenWindowsExplorer(tempdir())
 }
@@ -53,8 +75,8 @@ kwb.utils::loadFunctions(c(
 if (FALSE)
 {
   by_class <- split(
-    kwb.utils::removeColumns(full_teacher_data, c("class", "composed")),
-    f = full_teacher_data$class
+    kwb.utils::removeColumns(teacher_data, c("class", "composed")),
+    f = teacher_data$class
   )
   
   x <- by_class$`1A`
